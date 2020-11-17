1c436b2cc216d325b8c8dc920eba6375b9ae9be2
JarleSoelie <70138322+JarleSoelie@users.noreply.github.com>
2020-11-17 21:36:19 +0100

    Update createTree.cc

--- createTree.cc
+++ createTree.cc
@@ -31,15 +31,15 @@ using namespace std;
 vector<double> Energy;
 vector<double> dEdXBins;
 
-double findWET(double, double);
+double findWEPL(double, double);
 
 int main(int argc, char *argv[]) {
 
   int detectorVersion = 1; //*** 1 for ideal trackers, 2 for Bergen DTC
   
   //Variable definitions			
-  TVector3 TPSsource(0,-6600,0); //*** Position of scanning magnets [mm] and where proton angles are calculated from. Every position coming from GATE is in mm
-  float D_t = 50.0; //*** mm between trackers
+  TVector3 TPSsource(0,-6600,0); //*** [mm] Position of scanning magnets, where proton angles are calculated from. Every position coming from GATE is in mm
+  float D_t = 50.0; //*** [mm] distance between trackers
   float TPSx0, TPSy0, TPSz0, TPSpx0, TPSpy0, TPSpz0, 
         x11,y11,z11, x12,y12,z12, p1x,p1y,p1z, 
         x21,y21,z21, x22,y22,z22, p2x,p2y,p2z, 
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
   //-----------------Plan description with beam spot locations for TPS----------------------
   int NbProtonsperSpot;
   std::string linep;
-  std::ifstream TPSGen("../gate/PlanDescriptionToGate.txt"); //*** TPS-plan file
+  std::ifstream TPSGen("../gate/PlanDescriptionToGate.txt"); //*** TPS-plan file from GATE
   double datap[3];
   int j=0;
   while(getline(TPSGen, linep)) {
@@ -65,17 +65,17 @@ int main(int argc, char *argv[]) {
       SpotX.push_back(datap[0]);
       SpotZ.push_back(datap[1]);
     }
-    if (j==40) NbProtonsperSpot = datap[2]; //beam spot positions and weights start at line 40!
+    if (j==40) NbProtonsperSpot = datap[2]; //beam spot positions and weights start at line 40 in this file!
     j++;
   }  
   cout<<NbProtonsperSpot<<endl;
     
   //----------------------------
-  //For handling the proton WEPL. These .dat files must be prepared beforehand!
+  //For handling the proton WEPL. These .dat files must be prepared beforehand! (You can find them in this github)
   //----------------------------
   //For getting the WEPL from the MC calculated energy difference
   std::string line;
-  std::ifstream SPWater("Water_Proton.dat"); //file containing St.Pow. from underlying simulation (geant4 is made with 78eV!)
+  std::ifstream SPWater("Water_Proton.dat"); //file containing St.Pow. From the underlying Geant4 simulation (made with water ionization potential 78eV!)
   double data[2];
   while(getline(SPWater, line)) {
     stringstream ss(line);
@@ -148,7 +148,7 @@ int main(int argc, char *argv[]) {
   TTree *tOut = new TTree("OutTree", "Collection of tracker variables"); //***SET MANUALLY*** name of tree in the output root file 
   
 //----------------------Read data from Monte Carlo made root files--------------------------
-  //Front tracker data  
+  //Front tracker data (t11 is tracker one, t12 is tracker two BEFORE the phantom) 
   t11->SetBranchAddress("X",&x11); 
   t11->SetBranchAddress("Y",&y11);
   t11->SetBranchAddress("Z",&z11);
@@ -161,7 +161,7 @@ int main(int argc, char *argv[]) {
   
   t12->SetBranchAddress("Ekine", &Einit);
 
-  //Rear tracker data:
+  //Rear tracker data (t21 is tracker three, t22 is tracker four AFTER the phantom) 
   t21->SetBranchAddress("Ekine", &Estop);
   
   //Tracker21
@@ -180,11 +180,11 @@ int main(int argc, char *argv[]) {
   //Beam data:
   tOut->Branch("Einit",&Einit);  
   tOut->Branch("Estop",&Estop);
-  tOut->Branch("scatter_in",&scatter_in);
-  tOut->Branch("scatter_out",&scatter_out);
+  tOut->Branch("scatter_in",&scatter_in); //Scattering from front trackers (calculated later using Highland, but can be found using MC too...) 
+  tOut->Branch("scatter_out",&scatter_out); //Scattering from rear trackers (calculated later using Highland, but can be found using MC too... Though there is an energy dependence!)
   tOut->Branch("eventID",&IDexit22);  
-  tOut->Branch("WEPLideal",&WEPLideal);
-  tOut->Branch("WEPLreal",&WEPLreal);
+  tOut->Branch("WEPLideal",&WEPLideal); //Ideal wepl calculated using findWEPL function
+  tOut->Branch("WEPLreal",&WEPLreal); //This is either the WEPLideal repeated if ideal trackers is used, or the WEPLideal spread out using a Gaussian
     
   //TPS data:    
   tOut->Branch("TPSx0",&TPSx0);
@@ -221,7 +221,7 @@ int main(int argc, char *argv[]) {
   tOut->Branch("p2y",&p2y);
   tOut->Branch("p2z",&p2z);  
   
-  int Nprotons = t22->GetEntriesFast(); //Needs to be t22(exiting protons) otherwise the following for loop tries to find protons that has been lost 
+  int Nprotons = t22->GetEntriesFast(); //Needs to be t22(exiting protons) otherwise the following for-loop tries to find protons that has been lost 
   int enterID11 = 0; int enterID12 = 0; 
   int exitID21 = 0; int exitID22 = 0;
   int ProcessedSpots = 0;
@@ -232,98 +232,96 @@ int main(int argc, char *argv[]) {
 	//GetEntries for initializing!!
 	  t11->GetEntry(enterID11); 
 	  t12->GetEntry(enterID12);	  
-    t21->GetEntry(exitID21);
-    t22->GetEntry(exitID22); 
+          t21->GetEntry(exitID21);
+    	  t22->GetEntry(exitID22); 
 
     //We align everything to the protons that actually made it to the detector. Meaning highest eventID, which is "IDexit22"     
-	  while (IDexit22!=IDenter11 || IDexit22!=IDenter12 || IDexit22!=IDexit21){
+    while (IDexit22!=IDenter11 || IDexit22!=IDenter12 || IDexit22!=IDexit21){
       while(IDexit22<IDenter11 || IDexit22<IDenter12 || IDexit22<IDexit21){
-	    exitID22++;
-	    t22->GetEntry(exitID22); 
-	    }			
-	    while(IDexit22>IDenter11){
-	      enterID11++;
-	      t11->GetEntry(enterID11);
-        }
-        while(IDexit22>IDenter12){
-		    enterID12++;
-	      t12->GetEntry(enterID12);
-	    }
-	    while(IDexit22>IDexit21){
-		    exitID21++;
-		    t21->GetEntry(exitID21);
-	    }
+        exitID22++;
+	t22->GetEntry(exitID22); 
+      }			
+      while(IDexit22>IDenter11){
+        enterID11++;
+	t11->GetEntry(enterID11);
+      }
+      while(IDexit22>IDenter12){
+	enterID12++;
+	t12->GetEntry(enterID12);
+      }
+      while(IDexit22>IDexit21){
+	exitID21++;
+	t21->GetEntry(exitID21);
+      }
     }   
   //The proton is identified	  
-	if(IDexit22==IDenter11 && IDexit22==IDenter12 && IDexit22==IDexit21) {
+    if(IDexit22==IDenter11 && IDexit22==IDenter12 && IDexit22==IDexit21) {
 
-	  t11->GetEntry(enterID11);
-	  enterID11++;
-	  t12->GetEntry(enterID12);
-	  enterID12++;
-	  t21->GetEntry(exitID21);
-	  exitID21++;
-	  t22->GetEntry(exitID22);
-	  exitID22++;
+      t11->GetEntry(enterID11);
+      enterID11++;
+      t12->GetEntry(enterID12);
+      enterID12++;
+      t21->GetEntry(exitID21);
+      exitID21++;
+      t22->GetEntry(exitID22);
+      exitID22++;
 	  
-	  //Make sure we get the correct TPS spots from the plan description file
-	  if(IDexit22 >= NbProtonsperSpot*ProcessedSpots){   
-		  if(IDexit22 < NbProtonsperSpot*(ProcessedSpots+1)){
-			  TPSx0 = SpotX.at(ProcessedSpots);
-			  TPSz0 = SpotZ.at(ProcessedSpots);
-			  TPSy0 = 0.0; //mm ***SET MANUALLY*** The longitidunal position of the isocenter
-			  TVector3 TPSspot(TPSx0,TPSy0,TPSz0);
-		  	TVector3 dir = TPSspot - TPSsource;
-		  	dir.SetMag(1); //Normalizes the dir vector
+   //Make sure we get the correct TPS spots from the plan description file
+      if(IDexit22 >= NbProtonsperSpot*ProcessedSpots){   
+        if(IDexit22 < NbProtonsperSpot*(ProcessedSpots+1)){
+	  TPSx0 = SpotX.at(ProcessedSpots);
+	  TPSz0 = SpotZ.at(ProcessedSpots);
+	  TPSy0 = 0.0; //mm ***SET MANUALLY*** The longitidunal position of the isocenter
+	  TVector3 TPSspot(TPSx0,TPSy0,TPSz0);
+          TVector3 dir = TPSspot - TPSsource;
+	  dir.SetMag(1); //Normalizes the dir vector
 			
-			  TPSpx0 = dir.x();
-			  TPSpy0 = dir.y();
-			  TPSpz0 = dir.z();
+	  TPSpx0 = dir.x();
+	  TPSpy0 = dir.y();
+	  TPSpz0 = dir.z();
 			
-		   	//Project to tracker position, at -100 mm 
-			  TPSx0 = TPSx0 - ((TMath::Tan(TPSpx0))*100);
-			  TPSz0 = TPSz0 - ((TMath::Tan(TPSpz0))*100);
-			  TPSy0 = -100.0;
-		  } else {ProcessedSpots++;}
-    }
+	  //Project from iso-centre closer to tracker position, though I chose -100 mm to make sure I am outside the phantom 
+	  TPSx0 = TPSx0 - ((TMath::Tan(TPSpx0))*100);
+	  TPSz0 = TPSz0 - ((TMath::Tan(TPSpz0))*100);
+	  TPSy0 = -100.0;
+	} else {ProcessedSpots++;}
+      }
       
-    WEPLideal=findWET(Einit, Estop);  //Finding the ideal WEPL
-    if(WEPLideal>250.0)continue; //only have data up to 250 mm WEPL
+    WEPLideal=findWEPL(Einit, Estop);  //Finding the ideal WEPL
       
     if(detectorVersion==1){ //ideal detector
-		  WEPLreal = WEPLideal;   
-	    scatter_in = 0.0;
-	    scatter_out = 0.0;
+      WEPLreal = WEPLideal;   
+      scatter_in = 0.0; //No scattering in ideal trackers!
+      scatter_out = 0.0;
 	  
-	    p1x = atan((x12-x11)/D_t);
+      p1x = atan((x12-x11)/D_t); //calculating angles given tracker hits
       p1y = 1.0; //Forward facing
       p1z = atan((z12-z11)/D_t);  
     
       p2x = atan((x22-x21)/D_t);
       p2y = 1.0; //Forward facing
       p2z = atan((z22-z21)/D_t);
-	  } 
+    } 
       
     if(detectorVersion==2){ // Bergen detector
       //Bergen properties
-	    s_pos =0.005; //mm
-		  //Prepare for scattering calculation using Highland   
-		  matThick1 = 0.02; //cm of Carbon fiber
-      X01 = 25.723; //cm Carbon
-		  matBudget1 = matThick1/X01;
-		  matThick2 = 0.002; //cm of Silicon
-		  X02 = 9.365; //cm Silicon
-		  matBudget2 = matThick2/X02;
-		  //Calculate Sigma_scatter_in and out      
-		  scatter_in = 0.0; //no front tracker
-	
-	    //Calculate Sigma_scatter_out using Highland  
-	    double E = Estop;   
-	    double tau = E/938.3;
-	    double pv = ((tau+2)/(tau+1))*E;
-	    double scat_out1 = (14.1/(pv)*sqrt(matBudget1)*(1+((1/9)*log10(matBudget1))));
-	    double scat_out2 = (14.1/(pv)*sqrt(matBudget2)*(1+((1/9)*log10(matBudget2))));
-	    scatter_out = sqrt((scat_out1*scat_out1)+(scat_out2*scat_out2));
+      s_pos =0.005; //[mm] position uncertainty on hits! From ALPIDE documentation    
+      scatter_in = 0.0; //no front tracker!
+      //Calculate Sigma_scatter_out  	    
+      //Prepare for scattering calculation using Highland, only the material between the first tracker ALPIDE and the phantom is considered (See, Bopp et.al. and Krah et.al.)    
+      matThick1 = 0.02; //cm of Carbon fiber
+      X01 = 25.723; //cm Carbon radiation length
+      matBudget1 = matThick1/X01;
+      matThick2 = 0.002; //cm of Silicon
+      X02 = 9.365; //cm Silicon radiation length
+      matBudget2 = matThick2/X02;
+      //Calculate Sigma_scatter_out using Highland  
+      double E = Estop;   
+      double tau = E/938.3;
+      double pv = ((tau+2)/(tau+1))*E;
+      double scat_out1 = (14.1/(pv)*sqrt(matBudget1)*(1+((1/9)*log10(matBudget1))));
+      double scat_out2 = (14.1/(pv)*sqrt(matBudget2)*(1+((1/9)*log10(matBudget2))));
+      scatter_out = sqrt((scat_out1*scat_out1)+(scat_out2*scat_out2));
 
       //model new WEPL!
       int it_weplErr = lower_bound(WEPLsys.begin(), WEPLsys.end(), WEPLideal) - WEPLsys.begin();
@@ -336,32 +334,32 @@ int main(int argc, char *argv[]) {
       
       WEPLreal = gRandom->Gaus(WEPLdect, funnet_sigma); //Finding the modelled WEPL
       	
-	    //create the direction angles and uncertain positions using Gaussian based on position resolution from ALPIDE      
-	    double newXenter1 = gRandom->Gaus(x11, s_pos);
-	    double newZenter1 = gRandom->Gaus(z11, s_pos);      
-	    double newXenter2 = gRandom->Gaus(x12, s_pos);    
-	    double newZenter2 = gRandom->Gaus(z12, s_pos);     
-	    double newXexit1 = gRandom->Gaus(x21, s_pos);
-	    double newZexit1 = gRandom->Gaus(z21, s_pos);     
-	    double newXexit2 = gRandom->Gaus(x22, s_pos);   
-	    double newZexit2 = gRandom->Gaus(z22, s_pos);
-	
-	    p1x = atan((newXenter2-newXenter1)/D_t);
-	    p1y = 1.0; //Forward facing
-      	    p1z = atan((newZenter2-newZenter1)/D_t);         
+      //create the direction angles and uncertain positions using Gaussian based on position resolution from ALPIDE      
+      double newXenter1 = gRandom->Gaus(x11, s_pos);
+      double newZenter1 = gRandom->Gaus(z11, s_pos);      
+      double newXenter2 = gRandom->Gaus(x12, s_pos);    
+      double newZenter2 = gRandom->Gaus(z12, s_pos);     
+      double newXexit1 = gRandom->Gaus(x21, s_pos);
+      double newZexit1 = gRandom->Gaus(z21, s_pos);     
+      double newXexit2 = gRandom->Gaus(x22, s_pos);   
+      double newZexit2 = gRandom->Gaus(z22, s_pos);
+      	
+      p1x = atan((newXenter2-newXenter1)/D_t);
+      p1y = 1.0; //Forward facing
+      p1z = atan((newZenter2-newZenter1)/D_t);         
 		
-	    p2x = atan((newXexit2-newXexit1)/D_t);
-            p2y = 1.0; //Forward facing
-	    p2z = atan((newZexit2-newZexit1)/D_t);  
+      p2x = atan((newXexit2-newXexit1)/D_t);
+      p2y = 1.0; //Forward facing
+      p2z = atan((newZexit2-newZexit1)/D_t);  
 				
-	    x11 = newXenter1;
-	    x12 = newXenter2;
-	    z11 = newZenter1;
-	    z12 = newZenter2;
-	    x21 = newXexit1;
-	    x22 = newXexit2;
-	    z21 = newZexit1;
-	    z22 = newZexit2;     
+      x11 = newXenter1;
+      x12 = newXenter2;
+      z11 = newZenter1;
+      z12 = newZenter2;
+      x21 = newXexit1;
+      x22 = newXexit2;
+      z21 = newZexit1;
+      z22 = newZexit2;     
     }
 	          
     tOut->Fill(); //fill the tree with the data from latest GetEntries!
@@ -384,15 +382,15 @@ int main(int argc, char *argv[]) {
 }
 
 ////////////////////////////////////////////
-// Extract WET
+// Extract WEPL
 ////////////////////////////////////////////
-double findWET(double Einit,double Estop){
+double findWEPL(double Einit,double Estop){
   int it_Einit = lower_bound(Energy.begin(), Energy.end(), Einit)-Energy.begin();
   int it_Estop = lower_bound(Energy.begin(), Energy.end(), Estop)-Energy.begin();
   double step = Energy.at(1)-Energy.at(0);
-  double WET = 0 ;
+  double WEPL = 0 ;
   for(int i=it_Estop;i<it_Einit;i++){
-    WET += 1./dEdXBins[i];
+    WEPL += 1./dEdXBins[i];
   }
-  return WET*step; // to get WET in cm, divide this by 10;
+  return WEPL*step; // to get WEPL in cm, divide this by 10;
 }
